e7490f9f242b1fa2f3acafa0363a60c4662a59f0
osman-gif <osmanngong@gmail.com>
2024-07-14 11:00:40 +0200

    Added the 1-pack_web_static.py file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+from fabric.api import local
+import os
+from datetime import datetime
+
+def do_pack():
+    """Fabric script that generates a .tgz archive from the contents
+    of the web_static folder  function do_pack"""
+
+    if not os.path.exists('./versions'):
+        local('mkdir versions')
+
+    date = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive = 'versions/web_static_{}.tgz'.format(date)
+    result = local('tar -cvzf {} web_static'.format(archive))
+
+    if result.failed:
+        return None
+    else:
+        return archive
+
