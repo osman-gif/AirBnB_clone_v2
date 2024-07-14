429f8c8226cfe4fa7d07542bf02c61dbb8520759
osman-gif <osmanngong@gmail.com>
2024-07-14 13:39:49 +0200

    Modified the 2-do_deploy_web_static.py file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,19 +4,19 @@ from fabric.api import env, local, put, run
 env.hosts = ['ubuntu@54.162.48.101', 'ubuntu@100.26.167.222']
 env.key_filename = '~/.ssh/school'
 
+
 def do_deploy(archive_path):
     """A Fabric script (based on the file 1-pack_web_static.py) that
     distributes an archive to your web servers, using the function do_deploy"""
 
-    
     if not os.path.exists(archive_path):
         return False
     put("{}".format(archive_path), "/tmp/")
-    archive_path_server = "/tmp/{}".format(archive_path)
+    archive_path_server = "/tmp/{}".format(str(archive_path).split('/')[-1])
     filename = str(archive_path).split("/")[-1].split(".")[0]
-    if not run("mkdir -p /data/web_static/releases/{}".format(filename)):
+    if run("sudo mkdir -p /data/web_static/releases/{}".format(filename)):
         return False
-    if not run("tar -xzvf /data/web_static/releases/{}".format(filename)):
+    if run(f"tar -xvf {archive_path_server} -C /tmp/"):
         return False
     elif not run("rm -r {}".format(archive_path_server)):
         return False
