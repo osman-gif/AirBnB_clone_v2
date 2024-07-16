816287472a3758f7f7b13ed9b339d91e0f6f08d5
osman-gif <osmanngong@gmail.com>
2024-07-16 10:23:16 +0200

    Modified the 2-do_deploy_web_static.py file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,13 +18,18 @@ def do_deploy(archive_path):
     filename = str(archive_path).split("/")[-1].split(".")[0]
     uncompress_to = f"/data/web_static/releases/{filename}"
 
-    run(f"sudo mkdir -p {uncompress_to}")
-    run(f"sudo tar -xvf {remote_archive_path} -C {uncompress_to}")
-    # run(f"sudo mv -f {uncompress_to}/web_static/* {uncompress_to}")
-    run(f"sudo cp -r {uncompress_to}/web_static/* {uncompress_to}")
-    run(f"sudo rm -r {uncompress_to}/web_static/*")
-    run(f"sudo rm -r {uncompress_to}/web_static")
-    run(f"sudo rm -r {remote_archive_path}")
-    run("sudo rm /data/web_static/current")
-    run(f"sudo ln -s /data/web_static/releases/{filename} \
-                    /data/web_static/current")
+    try:
+        run(f"sudo mkdir -p {uncompress_to}")
+        run(f"sudo tar -xvf {remote_archive_path} -C {uncompress_to}")
+        # run(f"sudo mv -f {uncompress_to}/web_static/* {uncompress_to}")
+        run(f"sudo cp -r {uncompress_to}/web_static/* {uncompress_to}")
+        run(f"sudo rm -r {uncompress_to}/web_static/*")
+        run(f"sudo rm -r {uncompress_to}/web_static")
+        run(f"sudo rm -r {remote_archive_path}")
+        run("sudo rm /data/web_static/current")
+        run(f"sudo ln -s /data/web_static/releases/{filename} \
+                        /data/web_static/current")
+
+    except ValueError as e:
+        return False
+    return True
